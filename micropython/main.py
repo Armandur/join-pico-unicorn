6cbfd1932369670adfcaec21e72418c98d4e2744
Rasmus Pettersson Vik <rasmus.pettersson.vik@gmail.com>
2023-01-18 20:30:01 +0100

    Got a Waveshare UPS board, added support for showing battery percentage.

--- micropython/main.py
+++ micropython/main.py
@@ -1,37 +1,76 @@
-# Thanks to kevinmcaleer!
+# busyLight
+
 from scroller import Scroller
+from ina219 import INA219
+import gol
+
 import time
 import picounicorn
-import _thread
 import network
 import urequests
 import gc
 
-SSID = "Network"
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect(SSID, "")
+networks = [ ["SSID", "PASS"]]
 
+gc.enable()
+
+w = picounicorn.get_width()
+h = picounicorn.get_height()
+
+ina219 = INA219(addr=0x43)
+
+wlan = network.WLAN(network.STA_IF)
 scroll = Scroller()
 scroll.clear()
-scroll.brightness = 1
 
-message = ("Startup", 0)
-inCall = None
-
-states = {"color":"color", "showTime": "showTime"}
+inCall = False
+message = ""
 error = False
+blinkRed = False
+
+wifiTries = 3
 
 
-def setColor(color=(0, 0, 0)): # R G B
-    r = color[0]
-    g = color[1]
-    b = color[2]
+def scrollMessage(message, hue, speed):
+    global scroll
+    for position in range(16,-len(message*(5+1)),-1):
+        scroll.show_message(message, position, hue)
+        time.sleep(speed)
 
-    scroll.clear()
-    for col in range(16):
-        for row in range(7):
-            picounicorn.set_pixel(col, row, r, g, b)
+
+def connect():
+    global scroll
+    global wlan
+    global networks
+    global wifiTries
+    
+    wlan.active(True)
+    for ssid, password in networks:
+        wlan.connect(ssid, password)
+        for i in range(wifiTries):
+            if wlan.isconnected():
+                scrollMessage(f"Conn!", 0.3, 0.001)
+                scrollMessage(str(wlan.ifconfig()[0]), 0.3, 0.001)
+                print(wlan.ifconfig())
+                print(f"Connected to {ssid}!")
+                return
+            
+            error = False
+            scrollMessage(f"Conn {ssid} {i+1}/{wifiTries}", 0.06, 0.001)
+            time.sleep(1)
+    error = True
+    message = "No WiFi"
+
+
+def fillColor(color):
+    r, g, b = [255, 255, 255]
+    if color == "green":
+        r, g, b = [0, 255, 0]
+    if color == "red":
+        r, g, b = [255, 0, 0]
+    for x in range(w):
+        for y in range(h):
+            picounicorn.set_pixel(x, y, r, g, b)
 
 
 def checkStatus():
@@ -39,69 +78,143 @@ def checkStatus():
     global message
     global error
 
-    serverURL = "http://192.168.1.4:8585"
-    while True:
-        error = False
+    serverURL = "http://unicorn.pettersson-vik.se"
+    try:
         response = urequests.get(serverURL)
+        error = False
+    except Exception as e:
+        error = True
+        message = str(repr(e))
+        return
 
-        if response.status_code != 200:
-            message = (f"Error {response.status_code}", 0)
-            error = True
-
-            response.close()
-            gc.collect()
-            time.sleep(1)
-            continue
-
-        data = response.json()
-        inCall = data["inCall"]
-        message = (data["time"], 0)
-
+    if response.status_code != 200:
+        message = (f"Error {response.status_code}", 0)
+        error = True
+        
         response.close()
         gc.collect()
-        time.sleep(1)
+        return
+
+    data = response.json()
+    inCall = data["inCall"]
+    message = data["time"]
+    response.close()
+    gc.collect()
+    
+def hsv_to_rgb(h, s, v):
+    if s == 0.0:
+        return v, v, v
+    i = int(h * 6.0)
+    f = (h * 6.0) - i
+    p = v * (1.0 - s)
+    q = v * (1.0 - s * f)
+    t = v * (1.0 - s * (1.0 - f))
+    i = i % 6
+    if i == 0:
+        return v, t, p
+    if i == 1:
+        return q, v, p
+    if i == 2:
+        return p, v, t
+    if i == 3:
+        return p, q, v
+    if i == 4: 
+        return t, p, v
+    if i == 5:
+        return v, p, q
+
+def showRainbow():
+    time.sleep(1)
+    while not picounicorn.is_pressed(picounicorn.BUTTON_B):
+        t = time.ticks_ms() / 3600
+        for x in range(w):
+            for y in range(h):
+                r, g, b = [int(c * 255) for c in hsv_to_rgb(t + ((x + y) / w / 4), 1.0, 1.0)]
+                picounicorn.set_pixel(x, y, r, g, b)
 
 
-if __name__ == "__main__":
-    initMessage = "Initalizing..."
-    hue = 60
-
-    for position in range(16, -len(initMessage*(5+1)), -1):
-        scroll.show_message(initMessage, position, hue)
-        time.sleep(1)
-
-    statusThread = _thread.start_new_thread(checkStatus, ())
-    currentState = states["color"]
-    t = time.time()
-
-    initMessage = "Thread started..."
-
-    for position in range(16, -len(initMessage*(5+1)), -1):
-        scroll.show_message(initMessage, position, hue)
-        time.sleep(1)
-
-    while True:
-        if error:
-            scroll.clear()
-            for position in range(16, -len(currentMessage[0]*(5+1)), -1):
-                scroll.show_message(currentMessage[0], position, currentMessage[1])
-                time.sleep(1)
-
-        if currentState == states["color"]:
-            scroll.clear()
-            setColor((0, 255, 0))
-
-            if time.time() - t >= 5: #Show color for 5 seconds
-                currentState = states["showTime"]
-                continue
-
-        if currentState == states["showTime"]:
-            currentMessage = message
-            for i in range(3): #Scroll message three times
-                for position in range(16, -len(currentMessage[0]*(5+1)), -1):
-                    scroll.show_message(currentMessage[0], position, currentMessage[1])
-                    time.sleep(1)
-            currentState = states["color"]
-
+def showBattery():
+    global ina219
+    
+    time.sleep(1)
+    while not picounicorn.is_pressed(picounicorn.BUTTON_B):
+        percentage = (ina219.getBusVoltage_V() - 3)/1.2*100
+        if(percentage<0):
+            percentage=0
+        elif(percentage>100):
+            percentage=100
+            
+        percentage = round(percentage, 1)
+        
+        message = f"{percentage} %"
+        scroll.clear()
+        
+        scrollMessage(message, 0.06, 0.02)
+        print(message)
+        
         time.sleep(0.5)
-    scroll.clear()
+
+t = time.time()
+connect()
+
+rainbow = False
+gameOfLife = False
+battery = False
+
+while True or KeyboardInterrupt:
+    if rainbow:
+        showRainbow()
+        rainbow = False
+    
+    if gameOfLife:
+        gol.GameOfLife()
+        gameOfLife = False
+        
+    if battery:
+        showBattery()
+        battery = False
+    
+    if error:
+        scroll.clear()
+        scrollMessage(message, 0, 0.02)
+        time.sleep(0.1)
+        continue
+    
+    if not wlan.isconnected():
+        scroll.clear()
+        scrollMessage("No WiFi", 0, 0.02)
+        time.sleep(0.1)
+        connect()    
+    
+    checkStatus()
+    
+    if error:
+        scroll.clear()
+        scrollMessage(message, 0, 0.02)
+        time.sleep(0.1)
+        continue
+        
+    if not error and inCall:
+        blinkRed = not blinkRed
+        if blinkRed:
+            fillColor("red")
+        else:
+            scroll.clear()
+        if time.time() - t > 5:
+            scroll.clear()
+            strmsg = f"I samtal: {message}"
+            scrollMessage(strmsg, 0, 0.01)
+            t = time.time()       
+    else:
+        fillColor("green")
+        message = 0
+    time.sleep(0.1)
+    
+    if picounicorn.is_pressed(picounicorn.BUTTON_A):
+        rainbow = True
+        
+    if picounicorn.is_pressed(picounicorn.BUTTON_X):
+        gameOfLife = True
+        
+    if picounicorn.is_pressed(picounicorn.BUTTON_Y):
+        battery = True
